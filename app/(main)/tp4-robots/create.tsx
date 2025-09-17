b8b1589eb591a3349858725b79c6d3ed68451a06
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-09-17 14:02:30 +0200

    feat: view create robot

--- app/(main)/tp4-robots/create.tsx
+++ app/(main)/tp4-robots/create.tsx
@@ -1,3 +1,13 @@
-export default function CreateRobot () {
-    return;
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import RobotForm from "@/components/tp4-robots/RobotForm";
+
+export default function CreateRobot() {
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex: 1, padding: 16 }}>
+            <RobotForm onSubmitSuccess={() => router.push("/(main)/tp4-robots")} />
+        </SafeAreaView>
+    );
 }

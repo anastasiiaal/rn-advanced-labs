29186a9908452e618583d06ecddfca172f78518c
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-10-07 09:36:10 +0200

    fix: hooks & store

--- app/(tp4-robots-rtk)/hooks.ts
+++ app/(tp4-robots-rtk)/hooks.ts
@@ -1,6 +1,7 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState } from "./rootReducer";
-import type { AppDispatch } from "./store";
+import type { store } from "./store";
 
+export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- app/(tp4-robots-rtk)/store.ts
+++ app/(tp4-robots-rtk)/store.ts
@@ -1,24 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import rootReducer from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import { rootReducer } from "./rootReducer";
 
 const persistConfig = {
-    key: "robots-rtk-storage",
+    key: "root",
     storage: AsyncStorage,
+    // whitelist: ["robots"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: false, // redux-persist utilise des valeurs non sérialisables
-        }),
+    middleware: (gDM) => gDM({ serializableCheck: false }), // nécessaire pour redux-persist
 });
 
 export const persistor = persistStore(store);
-
 export type AppDispatch = typeof store.dispatch;
